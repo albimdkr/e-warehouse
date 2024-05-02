59c762ee912baae724fd6c6c0de5380aab31947d
albimdkr <albimudakar04@gmail.com>
2024-05-02 22:41:28 +0700

    feat: add sidebar> products out

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -18,10 +18,25 @@
         <span>Dashboard</span></a>
     </li>
     
-    <li class="nav-item">
+    {{-- <li class="nav-item">
       <a class="nav-link" href="{{ route('products') }}">
         <i class="fas fa-boxes"></i>
         <span>Products</span></a>
+    </li> --}}
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+          aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-boxes"></i>
+          <span>Products</span>
+      </a>
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+              <a class="collapse-item" href="{{ route('products') }}">Products In</a>
+              <a class="collapse-item" href="#">Products Out</a>
+          </div>
+      </div>
     </li>
     
     <li class="nav-item">
